8fe97310b661f7f16ba92ebebd04fe5c9a9fbb7c
Warizi <jjin9050@naver.com>
2023-04-17 01:50:22 +0900

    담당인재 등록 기능 구현

--- src/Components/ManagementPage/PersonInCharge/DnDBox/DndBox.js
+++ src/Components/ManagementPage/PersonInCharge/DnDBox/DndBox.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import styled from "styled-components";
-
-function DndBox ({type, dbArray}) {
+import COLOR from "../../../MainPage/COLOR";
+function ItemPick ({onClick, name }) {
+    const [colorToggle, setColorToggle] = useState('white');
+    function colorHandler () {
+        if (colorToggle === 'white') {
+            setColorToggle(COLOR.Primary);
+        } else {
+            setColorToggle('white');
+        }
+    }
+    return <ItemBox onClick={() => {onClick(); colorHandler();}} color={colorToggle} >{name}</ItemBox>
+};
+function DndBox ({type, dbArray, picker, setPicker, onClick }) {
+    //item을 클릭하고 해당 아이템이 picker에 있으면 삭제하고 없으면 추가합니다.
+    function pickToggle (name) {
+        const pick = picker.indexOf(name, 0);
+        if (pick === -1) {
+            const pushItem = picker;
+            pushItem.push(name);
+            setPicker([...pushItem]);
+        } else {
+            const deleteItem = picker;
+            deleteItem.splice(pick, 1);
+            setPicker([...deleteItem]);
+        }
+    }
     return <MainBox>
-        <SubmitBtn>{type}</SubmitBtn>
+        <SubmitBtn onClick={onClick}>{type}</SubmitBtn>
         <MainContent>
-            {dbArray.map((item, index) => {
-                return <ItemBox key={index}>{item}</ItemBox>
+            {dbArray.map((item) => {
+                return <ItemPick onClick={() => pickToggle(item)} key={item} name={item} />
             })}
         </MainContent>
     </MainBox>
@@ -13,7 +38,11 @@ function DndBox ({type, dbArray}) {
 const ItemBox = styled.div`
     width: 11rem;
     height: 5rem;
-    background-color: white;
+    background-color: ${props => props.color};
+    cursor: pointer;
+    text-align: center;
+    line-height: 5rem;
+    border-radius: 0.8rem;
 `;
 const MainContent = styled.div`
     overflow: scroll;
@@ -21,7 +50,7 @@ const MainContent = styled.div`
     height: 50rem;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: auto;
+    grid-auto-rows: 6rem;
     grid-gap: 1rem;
     background-color: green;
 `;
--- src/Components/ManagementPage/PersonInCharge/PersonInCharge.js
+++ src/Components/ManagementPage/PersonInCharge/PersonInCharge.js
@@ -3,14 +3,72 @@ import MainLayout from "../ManagementLayout/MainLayout";
 import BoxShadow from "../../MainPage/StyleComponents";
 import "./personInCharge.css";
 import DndBox from "./DnDBox/DndBox";
+import { useState } from "react";
 
-const personDB = ['강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현','강지용', '고기훈', '고재영', '오주연', '진승현'];
-
+const personDB = ['강성훈', '강인희', '강지용', '강지환', '고기훈', '고영준', '고재영', '김건우', '김광범', '김나연', '김남희', '김대한', '김동휘', '김로은', '김미정', '김민서', '김병준', '김보영', '김상훈', '김성서', '김소연', '김소은', '김정예', '김정원', '김지현', '김진성', '김진호', '김푸름', '김하정', '남병준', '문군호', '문승현', '문창현', '박경진', '박성영', '박세환', '변경환', '변재성', '송민혁', '송인정', '신지윤', '안수지', '양지선', '오주연', '원세종', '유승범', '이경헌', '이남호', '이수진', '이승현', '이재은', '이지현', '이채연', '이현아', '임소은', '임창현', '장재영', '장호', '정광호', '정루시아', '정유진', '정재석', '정지원', '조민지', '조송욱', '조수안', '조아라', '조윤수', '지예슬', '진승현', '최소영', '한주희', '황란경'];
+const selectDB = [];
 function PersonInChargeMove () {
+    //기존 서버 데이터
+    const [person, setPerson] = useState(personDB);
+    const [dropPerson, setDropPerson] = useState(selectDB);
+    //picker 데이터
+    const [pickPerson, setPickPerson] = useState([]);
+    const [pickDropPerson, setPickDropPerson] = useState([]);
+    function pickSubmit () {
+        const personArray = person;
+        const dropPersonArray = dropPerson;
+        const pickArray = pickPerson;
+        //기존 관리대상이 아닌 데이터에서 pick 데이터와 겹치는 요소를 삭제합니다.
+        for (let i = 0; i < pickArray.length; i++ ) {
+            const index = personArray.indexOf(pickArray[i], 0);
+            if (index !== -1) {
+                personArray.splice(index, 1);
+            };
+        };
+        //pick 데이터를 관리대상 데이터에 추가합니다.
+        for (let i = 0; i < pickArray.length; i++) {
+            //추후 push한 요소를 가나다 순으로 정렬해야 합니다. 
+            dropPersonArray.push(pickArray[i]);
+        }
+        //가나다 순으로 정렬합니다.
+        personArray.sort((a, b) => a.localeCompare(b));
+        dropPersonArray.sort((a, b) => a.localeCompare(b));
+        //마지막으로 state에 적용합니다.
+        setPerson([...personArray]);
+        setDropPerson([...dropPersonArray]);
+        //picker를 초기화 합니다.
+        setPickPerson([]);
+    };
+    function dropSubmit () {
+        //db의 종류를 모두 역순으로 적용했습니다.
+        const personArray = dropPerson;
+        const dropPersonArray = person;
+        const pickArray = pickDropPerson;
+        //기존 관리대상이 아닌 데이터에서 pick 데이터와 겹치는 요소를 삭제합니다.
+        for (let i = 0; i < pickArray.length; i++ ) {
+            const index = personArray.indexOf(pickArray[i], 0);
+            if (index !== -1) {
+                personArray.splice(index, 1);
+            };
+        };
+        //pick 데이터를 관리대상 데이터에 추가합니다.
+        for (let i = 0; i < pickArray.length; i++) {
+            //추후 push한 요소를 가나다 순으로 정렬해야 합니다. 
+            dropPersonArray.push(pickArray[i]);
+        }
+        //가나다 순으로 정렬합니다.
+        personArray.sort((a, b) => a.localeCompare(b));
+        dropPersonArray.sort((a, b) => a.localeCompare(b));
+        //마지막으로 state에 적용합니다.
+        setPerson([...dropPersonArray]);
+        setDropPerson([...personArray]);
+        //picker를 초기화 합니다.
+        setPickDropPerson([]);
+    };
     return <MainLayout content={
         <MainContainer>
             <div className="top_container">
-                <select value={'기수선택'}>
+                <select>
                     <option>8기</option>
                     <option>7기</option>
                     <option>6기</option>
@@ -22,9 +80,21 @@ function PersonInChargeMove () {
                 </select>
             </div>
             <div className="main_container">
-                <DndBox type={'플러스'} dbArray={personDB}/>
+                <DndBox 
+                    type={'플러스'} 
+                    dbArray={person} 
+                    onClick={pickSubmit} 
+                    picker={pickPerson} 
+                    setPicker={setPickPerson}
+                />
                 <div className="arrow"></div>
-                <DndBox type={'마이너스'} dbArray={personDB}/>
+                <DndBox 
+                    type={'마이너스'} 
+                    dbArray={dropPerson} 
+                    onClick={dropSubmit} 
+                    picker={pickDropPerson}
+                    setPicker={setPickDropPerson}
+                />
             </div>
         </MainContainer>
     }/>
